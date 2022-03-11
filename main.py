3d9b1658cc405d7e1e08ab4148501357b5d2b901
mcharipar <mcharipar5@gmail.com>
2022-03-11 00:33:08 -0600

    input_scree_and_return_forecast_in_console

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -12,13 +13,39 @@ class LoginScreen(GridLayout):
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text='Ticker: '))
+        self.ticker = TextInput(multiline=False)
+        self.inside.add_widget(self.ticker)
+        
+        self.inside.add_widget(Label(text='Start Data From: \n(YYYY-MM-DD)'))
+        self.start_date = TextInput(multiline=False)
+        self.inside.add_widget(self.start_date)
+        
+        self.inside.add_widget(Label(text='Days to Forecast: '))
+        self.days_to_forecast = TextInput(multiline=False)
+        self.inside.add_widget(self.days_to_forecast)
+
+        # Add inside as row 1/2
+        self.add_widget(self.inside)
+
+        self.submit = Button(text='Forecast', font_size=40)
+        self.submit.bind(on_press=self.press_forecast_button)
+        self.add_widget(self.submit)
+
+
+    def press_forecast_button(self, instance):
+        """
+        When Forecast button is pressed, YahooProphet is called and returns forecast to terminal.
+        """
+        print("Pressed Forecast Button")
+        yfp_obj = yfp.YahooProphet(self.ticker.text, self.start_date.text, int(self.days_to_forecast.text))
+        print(yfp_obj.forecast_df().tail(int(self.days_to_forecast.text)))
 
 
 class MyApp(App):
