b5bbb639975b2685047edef504124bb60e4bcaea
mcharipar <mcharipar5@gmail.com>
2022-03-15 17:49:10 -0500

    stop_using_kv_file

--- main.py
+++ main.py
@@ -11,49 +11,49 @@ warnings.filterwarnings("ignore")
 
 from ezekial import yahooprophet as yfp
 
-# class LoginScreen(GridLayout):
-class LoginScreen(Widget):
+class LoginScreen(GridLayout):
+# class LoginScreen(Widget):
 
-    # self.inside = Widget()
-    ticker = ObjectProperty(None)
-    start_date = ObjectProperty(None)
-    days_to_forecast = ObjectProperty(None)
+    # # self.inside = Widget()
+    # ticker = ObjectProperty(None)
+    # start_date = ObjectProperty(None)
+    # days_to_forecast = ObjectProperty(None)
 
-    # def __init__(self, **kwargs):
-    #     super(LoginScreen, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
         
-        # self.cols = 1
+        self.cols = 1
 
-        # self.inside = GridLayout()
-        # self.inside.cols = 2
+        self.inside = GridLayout()
+        self.inside.cols = 2
 
-        # self.inside.add_widget(Label(text='Ticker: '))
-        # self.ticker = TextInput(multiline=False)
-        # self.inside.add_widget(self.ticker)
+        self.inside.add_widget(Label(text='Ticker: '))
+        self.ticker = TextInput(multiline=False)
+        self.inside.add_widget(self.ticker)
         
-        # self.inside.add_widget(Label(text='Start Data From: \n(YYYY-MM-DD)'))
-        # self.start_date = TextInput(multiline=False)
-        # self.inside.add_widget(self.start_date)
+        self.inside.add_widget(Label(text='Start Data From: \n(YYYY-MM-DD)'))
+        self.start_date = TextInput(multiline=False)
+        self.inside.add_widget(self.start_date)
         
-        # self.inside.add_widget(Label(text='Days to Forecast: '))
-        # self.days_to_forecast = TextInput(multiline=False)
-        # self.inside.add_widget(self.days_to_forecast)
+        self.inside.add_widget(Label(text='Days to Forecast: '))
+        self.days_to_forecast = TextInput(multiline=False)
+        self.inside.add_widget(self.days_to_forecast)
 
-        # # Add inside as row 1/2
-        # self.add_widget(self.inside)
+        # Add inside as row 1/2
+        self.add_widget(self.inside)
 
-        # self.submit = Button(text='Forecast', font_size=40)
-        # self.submit.bind(on_press=self.press_forecast_button)
-        # self.add_widget(self.submit)
+        self.submit = Button(text='Forecast', font_size=40)
+        self.submit.bind(on_press=self.press_forecast_button)
+        self.add_widget(self.submit)
 
 
-    # def press_forecast_button(self, instance):
-    #     """
-    #     When Forecast button is pressed, YahooProphet is called and returns forecast to terminal.
-    #     """
-    #     print("Pressed Forecast Button")
-    #     yfp_obj = yfp.YahooProphet(self.ticker.text, self.start_date.text, int(self.days_to_forecast.text))
-    #     print(yfp_obj.forecast_df().tail(int(self.days_to_forecast.text)))
+    def press_forecast_button(self, instance):
+        """
+        When Forecast button is pressed, YahooProphet is called and returns forecast to terminal.
+        """
+        print("Pressed Forecast Button")
+        yfp_obj = yfp.YahooProphet(self.ticker.text, self.start_date.text, int(self.days_to_forecast.text))
+        print(yfp_obj.forecast_df().tail(int(self.days_to_forecast.text)))
 
 
 class MyApp(App):
